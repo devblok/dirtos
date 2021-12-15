00ea2a1d6d48bb35763e6c447ae4b9a5ce648975
devblok <l.praninskas@gmail.com>
2021-12-15 22:32:49 +0200

    Incomplete task.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
 
+var task_frame: @Frame(task) = undefined;
+
+fn task() void {
+    suspend {
+        task_frame = @frame();
+    }
+    std.log.info("All your codebase are belong to us.", .{});
+}
+
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
 }
